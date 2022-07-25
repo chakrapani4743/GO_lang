b88ab4b3ab29c5e8dc84facc47f6dfccae5c4974
chakrapani4743 <97500873+chakrapani4743@users.noreply.github.com>
2022-07-25 14:07:27 +0530

    added new file

---
+++ str_cmp.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+    str := "Veereshamma"
+    str1 := "Veereshamma"
+    if str == str1 {
+    fmt.Printf("Both are equal")
+    }else { 
+    fmt.Printf("Both are not equal")
+    }
+  //  fmt.Println(str)
+//    fmt.Println(len(str))
+}
